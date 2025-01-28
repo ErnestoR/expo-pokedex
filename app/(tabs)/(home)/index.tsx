3ddb8bcae2a5a276e054c2909f20032e21425215
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2025-01-28 14:08:39 -0600

    migrate to FlashList

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,7 +1,6 @@
 import {
   View,
   StatusBar,
-  VirtualizedList,
   ActivityIndicator,
   TouchableOpacity,
 } from 'react-native'
@@ -9,9 +8,9 @@ import { Image } from 'expo-image'
 import { Text } from 'react-native'
 import { Link } from 'expo-router'
 import * as Haptics from 'expo-haptics'
-
 import { useInfiniteQuery } from '@tanstack/react-query'
 import { fetchPokemons } from '@/api/pokemon'
+import { FlashList } from '@shopify/flash-list'
 
 export default function HomeScreen() {
   const {
@@ -52,7 +51,7 @@ export default function HomeScreen() {
       className="flex flex-col gap-2 flex-1 bg-neutral-50 dark:bg-slate-800 pt-14"
       style={{ marginTop: StatusBar.currentHeight || 0 }}
     >
-      <View className="flex flex-col gap-2 p-4 relative  ">
+      <View className="flex flex-col gap-2 p-4 relative">
         <Text
           className="text-5xl pt-8 text-slate-600 dark:text-neutral-100 tracking-[0.07em]"
           style={{
@@ -62,18 +61,16 @@ export default function HomeScreen() {
           Pokédex
         </Text>
       </View>
-      <VirtualizedList
+      <FlashList
         data={allPokemon}
-        initialNumToRender={20}
-        getItemCount={() => allPokemon.length}
-        getItem={(data, index) => data[index]}
+        estimatedItemSize={100} // Provide an estimated height for each item
         keyExtractor={(item) => item.url}
         renderItem={({ item, index }) => (
           <View className="bg-neutral-50 border-4 border-gray-200 rounded-2xl m-4 shadow-lg">
             <Link href={`/details/${item.name}`} asChild>
               <TouchableOpacity onPress={() => Haptics.selectionAsync()}>
                 <View className="flex flex-row items-center px-2 py-2 gap-6 ml-2">
-                  <View className="bg-gray-100 rounded-full w-20 h-20 flex items-center justify-center  ">
+                  <View className="bg-gray-100 rounded-full w-20 h-20 flex items-center justify-center">
                     <Image
                       source={{
                         uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
